9f9db41c29ca0b604b339d6a1f33c1c20006eaf9
carparnav3 <carparnav3@alu.edu.gva.es>
2023-12-03 21:13:05 +0100

    Primera version

---
+++ src/practica5.java
@@ -0,0 +1,27 @@
+
+
+//ESTA ES UNA PRACTICA ANTERIOR PARA LA UT5 DE ENTORNOS
+
+import java.util.Scanner;
+
+
+public class practica5 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+//PEDIMOS 2 NUMEROS
+        System.out.println("Introduce el primer número: ");
+        int primerNumero = scanner.nextInt();
+
+        System.out.println("Introduce el segundo número: ");
+        int segundoNumero = scanner.nextInt();
+
+        //MULTIPLICAMOS LOS NUMEROS Y MOSTRAMOS EL RESULTADO EN PANTALLA
+
+        int resultado = primerNumero * segundoNumero;
+
+        System.out.println("El resultado de la multiplicacion es: " + resultado);
+    }
+}
+
